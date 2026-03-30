cf14766cfa22a6d2a09da89a226d85dea8cf04a1
goqkfkrlk98-a11y <goqkfkrlk98@gmail.com>
2026-03-30 17:31:33 +0900

    컴포넌트 구조 완성

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Header from "./components/Header";
+import Header from "./components/Header";
+
+import Slider from "./components/Sider";
+import Image from "./components/Image";
+import ImageText from "./components/ImageText";
+import Card from "./components/Card";
+import Banner from "./components/Banner";
+import Text from "./components/Text";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Slider />
+      <Image />
+      <ImageText />
+      <Card />
+      <Banner />
+      <Text />
+    </>
+  );
+};
+
+export default App;
